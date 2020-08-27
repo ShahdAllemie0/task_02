402ac8e4a02c146d69f8b31906071ebde8c02a95
Shahd Allemie <shahdallemie@Shahds-MacBook-Pro.local>
2020-08-27 14:06:57 +0300

    Task2

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,2 +1,8 @@
 from django.shortcuts import render
+from django.http import Httpresponse
 
+def homepage (request):
+    context={
+    "msg":"hello CODED!!"
+    }
+    return render(request,home.html,context)
--- task_02/urls.py
+++ task_02/urls.py
@@ -19,5 +19,5 @@ from restaurants.views import x
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',,name='hello-world'),
+    path('Home',views.homepage,name='hello-world'),
 ]
